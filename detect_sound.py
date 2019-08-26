98959871354b3209de4be99812e1b5a76bcb2e39
aperkes <perkes.ammon@gmail.com>
2019-08-26 16:13:47 -0400

    Processing done, needs finetuning
    Need to figure out parameters
    Also actually start exporting wavs

--- detect_sound.py
+++ detect_sound.py
@@ -4,21 +4,50 @@
 
 # Import junk:
 import numpy as np
+from pydub import AudioSegment
+from scipy.io import wavfile as wf
+from scipy.signal import butter, lfilter
+from matplotlib import pyplot as plt
 
 # Set some parameters:
-SND_THRESH = 5
-SIL_THRESH = 3
+# These are in seconds:
+SND_THRESH = .5
+SIL_THRESH = .5
+AMP_THRESH = 500
+# Import wav (eventually this could run live (as ros?)) 
+wav_file = './annotated_wav_190213.wav'
+fs,wav_array = wf.read(wav_file)
+wav_audio = AudioSegment.from_wav(wav_file)
 
-# Import wav (eventually this could run live) 
+# Bandpass filter (see scipy cookbook)
+lowcut = 2000
+highcut = 20000
+order = 9 ## Might have to figure this out
+nyq = 0.5 * fs
+low = lowcut / nyq
+high = highcut / nyq
+## Filter
+print('filtering, this could take a minute...')
+b,a = butter(order, [low,high], btype='band')
+filter_array = lfilter(b,a,wav_array)
+print('done!')
 
-# Bandpass filter
-
-# Clip
-
+# Absolute value and clip
+print('clipping...')
+clip_array = np.abs(filter_array)
+clip_array = np.array(clip_array > AMP_THRESH).astype(int)
 ## Test with sample data:
-# High is non-inclusive, so for 1s, you want 2)
-wav_array = np.random.randint(0,high=2,size=1000)
+#wav_array = np.random.randint(0,high=2,size=1000)
 
+fig, (ax1,ax2) = plt.subplots(2)
+ax1.plot(clip_array[::1000])
+ax2.plot(wav_array[::1000])
+ax2.plot(filter_array[::1000])
+ax2.axhline(AMP_THRESH,color='r')
+fig.show()
+
+import pdb
+pdb.set_trace()
 def sound_clip(wav_array,pos_start,pos_stop):
 ## I could maybe have a part here to split it into manageable chunks...
     print(wav_array[pos_start:pos_stop + 1])
@@ -28,15 +57,17 @@ silence_count = 0
 counting_silence = False
 counting_sound = False
 sound_block = False
+
 # process at each sample
-for sample_idx in range(len(wav_array)):
-    sound = wav_array[sample_idx]
+print('processing sample:')
+for sample_idx in range(len(clip_array)):
+    sound = clip_array[sample_idx]
     if sound:
         silence_count = 0
         counting_silence = False
         if counting_sound:
             sound_count += 1
-            if sound_count == SND_THRESH:
+            if sound_count == SND_THRESH * fs:
                 counting_sound = False
                 sound_block = True
         elif sound_count == 0:
@@ -46,10 +77,12 @@ for sample_idx in range(len(wav_array)):
     else: ## i.e. silence
         if silence_count == 0:
             possible_stop = sample_idx
-        elif silence_count == SIL_THRESH:
+        elif silence_count == SIL_THRESH * fs:
             # It's a real stop! 
             sound_count = 0
             counting_sound = False
             if sound_block:
-                sound_clip(wav_array,possible_start,possible_stop)
+                print('clipping... at:',str(possible_start),str(possible_stop))
+                sound_clip(clip_array,possible_start,possible_stop)
+                sound_block = False
         silence_count += 1
