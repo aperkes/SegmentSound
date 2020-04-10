63cff2373be3862123b979ad7066b03031658919
aperkes <perkes.ammon@gmail.com>
2020-04-10 10:41:56 -0400

    Old update, tidying up

--- annotations_to_audio.py
+++ annotations_to_audio.py
@@ -11,9 +11,9 @@ from xml.etree import ElementTree as ET
 
 ## There's also a similar method using Scipy, which might be nicer
 
-wav_file = './aviary_2019-02-13-07-15-03_0_1550060103.wav'
+wav_file = './annotated_wav_190213.wav'
 wav_file_xml = wav_file.split('/')[-1]
-out_dir = './aviary_2019-02-13_segments/'
+out_dir = './inference_wavs'
 out_file = 'chunk_key.txt'
 annotation_file = './sabina_labels.txt'
 
@@ -37,6 +37,16 @@ course_to_int_labels = {
     'w': 6,
     'n': 7}
 
+course_to_int_binary = {
+    'b': 0,
+    'c': 0,
+    's': 0,
+    't': 0,
+    'o': 0,
+    'h': 0,
+    'w': 0,
+    'n': 1}
+
 ## Open wav file
 song_info = mediainfo(wav_file)
 s_rate = song_info['sample_rate']
@@ -52,7 +62,9 @@ with open(annotation_file,'r') as annotation_obj:
         line_split = line.strip().split('\t')
         start,stop,annot = line_split
         if start == stop:
-            continue
+            #continue
+            start = float(start) - 1
+            stop = float(stop) + 1
         annot_course = fine_to_course_labels[annot]
         annot_int = str(course_to_int_labels[annot_course])
         start_hz = int(float(start) * int(s_rate))
